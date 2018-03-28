87784b606343cbda82b4748d5ddfeabe8fa71c92
Elcoosp <elcoosp@gmail.com>
2018-03-28 22:01:32 +0200

    Create posts and tags pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const { kebabCase, get, each, uniq } = require('lodash')
 
 exports.createPages = ({ boundActionCreators: { createPage }, graphql }) => {
-  const component = path.resolve(`src/components/Post/index.js`)
+  const PostComponent = path.resolve(`src/components/Post/index.js`)
+  const TagPageComponent = path.resolve(`src/components/TagPage/index.js`)
 
   return graphql(`
     {
@@ -13,6 +15,7 @@ exports.createPages = ({ boundActionCreators: { createPage }, graphql }) => {
           node {
             frontmatter {
               path
+              tags
             }
           }
         }
@@ -20,13 +23,35 @@ exports.createPages = ({ boundActionCreators: { createPage }, graphql }) => {
     }
   `).then(({ errors, data }) => {
     if (errors) return Promise.reject(errors)
+    const posts = data.allMarkdownRemark.edges
 
-    data.allMarkdownRemark.edges.forEach(({ node }) => {
+    // Posts pages
+    posts.forEach(({ node }) => {
       createPage({
         path: node.frontmatter.path,
-        component,
-        context: {},
+        component: PostComponent,
       })
     })
+
+    // Tag pages
+    let tags = []
+
+    each(posts, edge => {
+      if (get(edge, 'node.frontmatter.tags')) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+
+    tags = uniq(tags)
+
+    tags.forEach(tag =>
+      createPage({
+        path: `/tags/${kebabCase(tag)}/`,
+        component: TagPageComponent,
+        context: {
+          tag,
+        },
+      })
+    )
   })
 }
